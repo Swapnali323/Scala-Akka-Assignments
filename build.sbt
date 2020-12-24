3b844b7a51a786aac3af7ebf3cebcd4e0f4897ea
swapnali jadhav <swapnali123@gmail.com>
2020-12-24 18:49:01 +0530

    assignment

--- build.sbt
+++ build.sbt
@@ -11,9 +11,13 @@ val akkaHttpVersion = "10.2.2"
 
 libraryDependencies ++=Seq(
   "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+  "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
+  "org.scalatest" %% "scalatest" % "3.2.2",
   "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion,
   "com.typesafe.akka" %% "akka-stream" % akkaVersion,
   "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
-  "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion
+  "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion,
+//  "org.slf4j" % "slf4j-api" % "1.7.9", "org.slf4j" % "slf4j-simple" % "1.7.8",
+  "ch.qos.logback" % "logback-classic" % "1.2.3" % Runtime
 )
 
--- src/main/scala/routing/HttpRoutingAkka.scala
+++ src/main/scala/routing/HttpRoutingAkka.scala
@@ -1,51 +1,113 @@
 package routing
 
+import akka.actor.{Actor, ActorLogging, Props, Terminated}
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
+
+import akka.event.Logging
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model.HttpRequest
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.directives.LogEntry
+import akka.http.scaladsl.server.{Route, RouteResult}
 import routing.HttpRoutingAkka.{Person, UserAdded}
 import spray.json._
-
+import com.typesafe.config.ConfigFactory
+import com.typesafe.config.ConfigFactory
+import scala.concurrent.duration._
 import java.util.UUID
 
+
 trait PersonJsonProtocol extends DefaultJsonProtocol {
-  /*
-    The `jsonFormat2` method is applicable for Product types (which include case classes) with 2 members.
-    If you have case classes with more than 2 fields, you'd use the appropriate `jsonFormatX` method.
-  */
   implicit val personFormat = jsonFormat2(Person)
   implicit val userAddedFormat = jsonFormat1(UserAdded)
 }
 
-
 object HttpRoutingAkka extends PersonJsonProtocol with SprayJsonSupport {
-  case class Person(name: String, id: Int)
-  case class UserAdded(secretCode: String)
 
-  implicit val system = ActorSystem(Behaviors.empty, "AkkaHttpJson")
+  class SimpleLoggingActor extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(message.toString)
+    }
+  }
 
-  val route: Route =
-    concat(
-      get {
-        path("api" / "user" / LongNumber) {id=>
-          complete("This is a get request " + id)
-        }
-      },
-      post {
-        path("api" / "user") {
-          entity(as[Person]) { person =>
-            complete(UserAdded(UUID.randomUUID().toString))
+
+    case class Person(name: String, id: Int)
+
+    case class UserAdded(secretCode: String)
+
+   implicit val system = ActorSystem(Behaviors.empty, "AkkaHttpJson")
+  val config = ConfigFactory.load().getConfig("mySpecialConfig")
+
+  println(s"  ${config.getString("akka.greet")}")
+
+
+    val route: Route =
+      concat(
+        get {
+          path("api" / "user" / IntNumber) { id =>
+
+            extractLog{
+              log=>{
+            val startTime: Long = System.currentTimeMillis()
+            log.info(s"start time $startTime")
+
+            extractUri { uri =>
+              validate(uri.path.toString.size < 15, s"incorrect path:${uri.path.toString}") {
+
+                val endTime: Long = System.currentTimeMillis()
+
+                log.info(s"end time $endTime")
+
+                val time = endTime - startTime
+                log.info(s"time taken $time")
+                complete {
+                  (
+                    "Welcome to Akka ",
+                    s"This is a get request from $id ",
+                  )
+                }
+              }
+            }
+          }
+            }
+          }
+          },
+        post {
+          path("api" / "user") {
+            entity(as[Person]) { person =>
+              extractLog {
+                log => {
+                  val startTime: Long = System.currentTimeMillis()
+                  log.info(s"start time $startTime")
+
+                  validate(person.id.toString.size > 0 && person.id.toString.size < 5, "Id entered is wrong") {
+                    val endTime: Long = System.currentTimeMillis()
+                    log.info(s"end time $endTime")
+
+                    val time = endTime - startTime
+                    log.info(s"time taken $time")
+                    complete {
+                      (
+                        UserAdded(UUID.randomUUID().toString)
+                        )
+                    }
+
+
+                  }
+
+                }
+              }
+            }
           }
         }
-      }
-    )
+      )
+
+    def main(args: Array[String]): Unit = {
+      Http().newServerAt("localhost", 8081).bind(route)
+
+    }
 
 
-  def main(args: Array[String]): Unit = {
-    Http().newServerAt("localhost", 8081).bind(route)
   }
-}
-
